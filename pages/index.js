99eb8d8375651c0cc381703514dbad9d4434b525
Swarnim37 <swarnimsharma37@gmail.com>
2022-08-08 19:56:58 +0200

    meta

--- pages/index.js
+++ pages/index.js
@@ -9,11 +9,11 @@ export default function Home() {
         <title>Astrologie Sanatan</title>
         <meta
           name="description"
-          content="Nabízíme tyto služby - výklad osobního horoskopu, konzultace současných trendů a vývoj v blízké budoucnosti, výběr vhodné doby pro začátek podnikání, operaci, cestování, apod."
+          content="Astrologie Sanatan, Nabízíme tyto služby - výklad osobního horoskopu, konzultace současných trendů a vývoj v blízké budoucnosti, výběr vhodné doby pro začátek podnikání, operaci, cestování, apod."
         />
         <meta
           name="Keywords"
-          content="  výklad horoskopu, astrologické konzultace, váš védský horoskop "
+          content=" Astrologie Sanatan, výklad horoskopu, astrologické konzultace, váš védský horoskop "
         />
         <link rel="icon" href="/img/logosvg.svg" />
       </Head>
